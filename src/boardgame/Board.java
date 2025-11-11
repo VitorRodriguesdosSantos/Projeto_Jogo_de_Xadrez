e5b4de19e8f663c09ecaf39b42577230301ee040
Vitor Rodrigues dos Santos <164433069+VitorRodriguesdosSantos@users.noreply.github.com>
2025-11-11 09:48:24 -0300

    Camada de xadrez e impressão do tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,29 +1,37 @@
 package boardgame;
 
 public class Board {
-    private int rows;
-    private int columns;
+    private Integer rows;
+    private Integer columns;
     private Piece[][] pieces;
 
-    public Board(int rows, int columns) {
+    public Board(Integer rows, Integer columns) {
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
     }
 
-    public int getRows() {
+    public Piece piece(Integer row, Integer column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public Integer getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
+    public void setRows(Integer rows) {
         this.rows = rows;
     }
 
-    public int getColumns() {
+    public Integer getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
+    public void setColumns(Integer columns) {
         this.columns = columns;
     }
 }
