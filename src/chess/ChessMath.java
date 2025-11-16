26ae2a07cbdcc145624c769b0f214d7ac9e614f9
Vitor Rodrigues dos Santos <164433069+VitorRodriguesdosSantos@users.noreply.github.com>
2025-11-16 17:56:21 -0300

    Imprimindo os possiveis movimentos

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -64,6 +64,12 @@ public class ChessMath {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     private void initialSetup() {
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
